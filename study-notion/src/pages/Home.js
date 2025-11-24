de1b95e50543b6bd253bb5e627fbcd1f9c6df005
Ujjwal092 <us581318@gmail.com>
2025-11-24 21:51:28 +0530

    Initial commit

---
+++ study-notion/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = ({ isLoggedIn}) => {
+  return (
+    <div className="flex justify-center items-center text-white text-3xl h-full">
+      Home
+    </div>
+    //h-full coz wo div hai uska height pura krdega
+  );
+};
+
+export default Home;
