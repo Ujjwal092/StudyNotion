ab7b4a7d9737fbfdfe3c69fca7c3ca575e09c18a
Ujjwal092 <us581318@gmail.com>
2026-03-28 22:49:43 +0530

    commit

--- Server/utils/mailSender.js
+++ Server/utils/mailSender.js
@@ -7,7 +7,7 @@ const mailSender = async (email, title, body) => {
       {
         sender: {
           name: "StudyNotion",
-          email: "us581318@gmail.com", // verified sender
+          email: "us581318@gmail.com",
         },
         to: [{ email }],
         subject: title,
@@ -15,14 +15,13 @@ const mailSender = async (email, title, body) => {
       },
       {
         headers: {
-          "api-key": process.env.MAIL_PASS,
+          "api-key": process.env.BREVO_API_KEY,
           "Content-Type": "application/json",
         },
       },
     );
-    console.log("API KEY:", process.env.MAIL_PASS);
+
     console.log("MAIL SENT:", response.data);
-    return response.data;
   } catch (error) {
     console.log("MAIL ERROR:", error.response?.data || error.message);
   }
